f9e276166dc9ca2cbe0a596ebf043cb371e74603
kert <franciskertdn@gmail.com>
2025-09-04 23:29:35 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/mainpage.css') }}">
+    <title>Tour_Pointing_System</title>
+</head>
+<body>
+    @include('partials._nav')
+    <div class="card shadow mb-3 d-flex justify-content-center align-items-center">
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
